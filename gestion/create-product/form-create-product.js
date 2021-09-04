1e201fadc44746339be49e388d720967ad466530
veronica romero <vdrr89@gmail.com>
2021-09-04 00:07:33 -0300

    login y log out divs maquetados en html y css

--- gestion/create-product/form-create-product.js
+++ gestion/create-product/form-create-product.js
@@ -5,12 +5,17 @@ var btnSend = document.getElementById("btn-send");
 
 //inputs
 var titulo = document.getElementById("titulo");
+var imgMiniDiv = document.getElementById("imgmini-input");
 var imageurl = document.getElementById("imageurl");
+var videoMiniDiv = document.getElementById("videomini-input");
 var videourl = document.getElementById("videourl");
 var precio = document.getElementById("precio");
 var cantidadu = document.getElementById("cantidadu");
 var stock = document.getElementById("stock");
 var descripcion = document.getElementById("descripcion");
+
+imgMiniDiv.innerHTML('<img src="' + imageurl + '" alt="' + titulo + '">');
+videoMiniDiv.innerHTML('<video src="' + videourl + '" alt="' + titulo + '"></video>');
 
 //no funciona
 // if(titulo.value && imageurl.value && videourl.value && precio.value && cantidadu.value && stock.value && descripcion.value != ""){
@@ -29,6 +34,26 @@ var descripcion = document.getElementById("descripcion");
 //         btnDelete.style.display = "none";
 //     }
 // }
+
+//no funciona
+// let elementos = document.getElementsByClassName("form-floating");
+
+
+// function ifEmptyInput(){
+//     var validado = true;
+//     for(i=0;i<elementos.length;i++){
+//       if(elementos[i].value == "" || elementos[i].value == null){
+//         btnBack.style.display = "block";
+//         btnDelete.style.display = "none";
+//       } else{
+//         btnBack.style.display = "none";
+//         btnDelete.style.display = "block";
+//         }
+//     }
+// }
+
+// elementos.addEventListener("keyup", ifEmptyInput());
+
 
 //no funcionan
 titulo.addEventListener("keyup", changeBackDelete());
