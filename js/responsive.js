c6f4b65a1bb37954b4a5ddde1191e336a03de335
veronica romero <vdrr89@gmail.com>
2021-09-04 20:59:57 -0300

    menu en responsive prolijo

--- js/responsive.js
+++ js/responsive.js
@@ -5,11 +5,13 @@ var hamburguerBtn = document.getElementById("hamburguer-menu-icon");
 var xBtn = document.getElementById("x-menu-icon");
 
 var responsiveMenu = document.getElementById("responsive-menu");
+var menuBackground = document.getElementById("responsive-menu-cover");
 
 function openMenu(){
     hamburguerBtn.style.display = "none";
     xBtn.style.display = "block";
     responsiveMenu.style.display = "block";
+    menuBackground.style.display = "block";
     console.log("openMenu()");
 }
 
@@ -17,5 +19,7 @@ function closeMenu(){
     hamburguerBtn.style.display = "block";
     xBtn.style.display = "none";
     responsiveMenu.style.display = "none";
+    menuBackground.style.display = "none";
     console.log(closeMenu());
 }
+
